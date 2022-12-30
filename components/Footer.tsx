3f43ea7598ff6ad0fc7dd4b07fe71975e28a9b7a
paul_gedeon <paul.gedeon@redalertlabs.com>
2022-12-30 20:08:25 +0100

    initialisation

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer>
+      Made by <a href="https://twitter.com/0xedeon">0xedeon</a>. Open source project for BSC, see{' '}
+      <a href="https://github.com/paulgg-code/abi.bsc">GitHub</a>.<br />
+      Powered by <a href="https://github.com/shazow/whatsabi">@shazow/whatsabi</a> and{' '}
+      <a href="https://sig.eth.samczsun.com/">Ethereum Signature Database</a>.      
+      Foked from <a href="https://twitter.com/w1nt3r_eth">@w1nt3r_eth</a>.
+      <style jsx>{`
+        footer {
+          margin-top: 2rem;
+        }
+      `}</style>
+    </footer>
+  );
+}
